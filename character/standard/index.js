7dd74a46bffb31f1adae376d6d5fc9303dcff409
シカト·チイカワスケ <153523152+Cicardo-Thicavasco@users.noreply.github.com>
2025-10-12 23:46:42 +0800

    Update index.js
    回滚了原来标包下index导入的characteTitle.js功能

--- character/standard/index.js
+++ character/standard/index.js
@@ -9,7 +9,6 @@ import characterFilters from "./characterFilter.js";
 import dynamicTranslates from "./dynamicTranslate.js";
 import perfectPairs from "./perfectPairs.js";
 import voices from "./voices.js";
-import characterTitle from "../characterTitle.js";
 import { characterSort, characterSortTranslate } from "./sort.js";
 
 game.import("character", function () {
@@ -28,6 +27,5 @@ game.import("character", function () {
 		perfectPair: { ...perfectPairs },
 		translate: { ...translates, ...voices, ...characterSortTranslate },
 		pinyins: { ...pinyins },
-		characterTitle: { ...characterTitle },
 	};
 });
