43bbd1b4ae6583834c2d026e9c88fc52b64c031a
シカト·チイカワスケ <153523152+Cicardo-Thicavasco@users.noreply.github.com>
2025-10-13 09:50:35 +0800

    Update index.js

--- noname/init/index.js
+++ noname/init/index.js
@@ -702,6 +702,7 @@ export async function boot() {
 	toLoad.push(lib.init.promises.js(`${lib.assetURL}character`, "rank"));
 	toLoad.push(lib.init.promises.js(`${lib.assetURL}character`, "replace"));
 	toLoad.push(lib.init.promises.js(`${lib.assetURL}character`, "perfectPairs"));
+	toLoad.push(lib.init.promises.js(`${lib.assetURL}character`, "characterTitle"));
 
 	if (_status.javaScriptExtensions) {
 		const loadJavaScriptExtension = async (javaScriptExtension, pathArray, fileArray, onLoadArray, onErrorArray, index) => {
