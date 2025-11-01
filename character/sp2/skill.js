951a8072d56705420787ee5f0f2f1685b6b40740
Zhi_liao <164357750+Xiazhiliao@users.noreply.github.com>
2025-11-01 22:43:39 +0800

    〖荡尘〗fix
    修复技能效果持续时长错误的问题（本回合→本阶段），更正对应技能描述；

--- character/sp2/skill.js
+++ character/sp2/skill.js
@@ -796,7 +796,7 @@ const skills = {
 				if (!result?.bool) {
 					return;
 				}
-				player.addTempSkill("stardangchen_buff");
+				player.addTempSkill("stardangchen_buff", { player: "phaseUseEnd" });
 				player.addMark("stardangchen_buff", num, false);
 			}
 		},
