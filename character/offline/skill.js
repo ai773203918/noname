c288efefa3a53b035e23b7766cf80cbd6b22bf2f
AstralBarrage <hoshimimaidono@gmail.com>
2025-11-08 21:32:29 +0800

    地道改为限定技

--- character/offline/skill.js
+++ character/offline/skill.js
@@ -16734,33 +16734,40 @@ const skills = {
 	},
 	//严政
 	hm_didao: {
-		locked: true,
 		trigger: {
 			player: "phaseUseBegin",
 		},
 		filter(event, player) {
 			return player.isDamaged() && player.countCards("h");
 		},
-		check(event, player) {
-			const card = get.autoViewAs({ name: "sha" }, player.getCards("h"));
-			return player.hasValueTarget(card, false, false);
-		},
-		async content(event, trigger, player) {
-			await player
+		limited: true,
+		skillAnimation: true,
+		animationColor: "metal",
+		async cost(event, trigger, player) {
+			event.result = await player
 				.chooseToUse()
-				.set("openskilldialog", "地道：将所有手牌当一张无距离限制的伤害为X的【杀】使用(X为此【杀】的实体牌数）")
+				.set("openskilldialog", get.prompt2(event.skill))
 				.set("norestore", true)
-				.set("_backupevent", `${event.name}_backup`)
+				.set("_backupevent", `${event.skill}_backup`)
 				.set("custom", {
 					add: {},
 					replace: { window() {} },
 				})
-				.backup(`${event.name}_backup`)
-				.set("oncard", () => {
-					const evt = get.event();
-					evt.baseDamage = evt.cards.length;
-				})
-				.set("addCount", false);
+				.backup(`${event.skill}_backup`)
+				.set("addCount", false)
+				.set("chooseonly", true)
+				.forResult();
+		},
+		async content(event, trigger, player) {
+			const { result } = event.cost_data;
+			const next = player.useResult(result, event);
+			player
+				.when("useCard0")
+				.filter(evt => evt == next)
+				.step(async (event, trigger, player) => {
+					trigger.baseDamage = trigger.cards.length;
+				});
+			await next;
 		},
 		subSkill: {
 			backup: {
