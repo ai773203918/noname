b6b45e75eda78dfec5bdb3c8e5e14220c2ccb597
xiyang141 <3495404507@qq.com>
2025-12-07 17:09:31 +0800

    extraEquip类型检查修正

--- noname/library/element/player.js
+++ noname/library/element/player.js
@@ -196,7 +196,7 @@ export class Player extends HTMLDivElement {
 		}
 	}
 	/**
-	 * @type { Array<[string,string,number]|[string,string,number,Function]> }
+	 * @type { Array<Array<string,string,number,Function|undefined>> }
 	 */
 	extraEquip;
 	/** @type { SMap<HTMLDivElement> } */
