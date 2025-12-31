167e5d8197902fc7413e1e5f920f4773f55b8a4c
xjm030708 <814805038@qq.com>
2025-12-31 15:07:11 +0800

    十常侍logSkill的时机调前

--- character/mobile/skill.js
+++ character/mobile/skill.js
@@ -13353,7 +13353,6 @@ const skills = {
 			}
 			else if (event.triggername == "dieAfter") {
 				if (player.getStorage("mbdanggu").length) {
-					player.logSkill("mbmowang");
 					game.broadcastAll(function () {
 						if (lib.config.background_speak) {
 							game.playAudio("die", "shichangshiRest");
@@ -13367,6 +13366,7 @@ const skills = {
 					trigger.cancel();
 				} else {
 					if (player.getStorage("mbdanggu").length) {
+						player.logSkill("mbmowang");
 						//煞笔十常侍
 						trigger.excludeMark.add("mbdanggu");
 						trigger.noDieAudio = true;
