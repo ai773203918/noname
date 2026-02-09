b7a94c93dee911200eef8439cae937612b001401
xjm030708 <814805038@qq.com>
2026-02-09 22:08:49 +0800

    新杀谋周瑜、谋许攸、OL周群技能调整

--- apps/core/character/xianding/skill.js
+++ apps/core/character/xianding/skill.js
@@ -1182,7 +1182,7 @@ const skills = {
 		},
 		prompt2(event, player) {
 			const targets = get.info("dcsbyinmou").getTargets();
-			return `${get.skillInfoTranslation("dcsbyinmou", player, false)}<br><span class=bluetext>可选目标：${get.translation(targets)}</span>`
+			return `${get.skillInfoTranslation("dcsbyinmou", player, false)}<br><span class=bluetext>可选目标：${get.translation(targets)}</span>`;
 		},
 		check(event, player) {
 			const targets = get.info("dcsbyinmou").getTargets();
@@ -2210,21 +2210,30 @@ const skills = {
 					const cards = getCards(suit);
 					await player.discard(cards);
 					if (
-						hs
-							.removeArray(cards)
+						!player
+							.getCards("h")
 							.map(card => get.type2(card))
-							.unique().length < types.length
+							.containsAll("basic", "trick", "equip")
 					) {
-						player.addTempSkill(`${event.name}_effect`);
+						player.addTempSkill(`${event.name}_basic`);
+						player.addTempSkill(`${event.name}_trick`);
 					}
 				}
 			}
 		},
 		subSkill: {
-			effect: {
+			basic: {
 				mod: {
-					cardUsable: () => Infinity,
-					targetInRange: () => true,
+					cardUsable(card, player) {
+						if (get.type(card) == "basic") {
+							return Infinity;
+						}
+					},
+					targetInRange(card, player) {
+						if (get.type(card) == "basic") {
+							return true;
+						}
+					},
 				},
 				trigger: {
 					player: "useCard1",
@@ -2233,7 +2242,10 @@ const skills = {
 				charlotte: true,
 				popup: false,
 				firstDo: true,
-				content() {
+				filter(event, player) {
+					return get.type2(event.card) == "basic";
+				},
+				async content(event, trigger, player) {
 					player.removeSkill(event.name);
 					if (trigger.addCount !== false) {
 						trigger.addCount = false;
@@ -2245,8 +2257,27 @@ const skills = {
 					}
 				},
 				mark: true,
+				marktext: "基",
 				intro: {
-					content: "使用下一张牌无距离和次数限制",
+					content: "使用下一张基本牌无距离和次数限制",
+				},
+			},
+			trick: {
+				charlotte: true,
+				forced: true,
+				trigger: { player: "useCard" },
+				filter(event, player) {
+					return get.type2(event.card) == "trick";
+				},
+				async content(event, trigger, player) {
+					player.removeSkill(event.name);
+					game.log(trigger.card, "不可被响应");
+					trigger.directHit.addArray(game.players);
+				},
+				mark: true,
+				marktext: "锦",
+				intro: {
+					content: "使用下一张锦囊牌不可响应",
 				},
 			},
 		},
@@ -5011,7 +5042,6 @@ const skills = {
 					}
 					return 7 - get.value(card);
 				})
-				//.set("chooseonly", true)
 				.forResult();
 		},
 		async content(event, trigger, player) {
@@ -18996,10 +19026,70 @@ const skills = {
 			return (event.card.name == "sha" && game.hasNature(event.card, "fire")) || event.card.name == "huogong";
 		},
 		forced: true,
-		content() {
-			trigger.baseDamage = game.countGroup();
+		async content(event, trigger, player) {
+			trigger.baseDamage += game.countGroup();
 		},
 		ai: { threaten: 3.5 },
+		group: ["dcsbronghuo_target"],
+		subSkill: {
+			target: {
+				audio: "dcsbronghuo",
+				forced: true,
+				trigger: {
+					global: "damageEnd",
+				},
+				filter(event, player) {
+					return event.hasNature("fire");
+				},
+				async content(event, trigger, player) {
+					player.addTempSkill("dcsbronghuo_effect", { player: "dieAfter" });
+					player.addMark("dcsbronghuo_effect", 1, false);
+				},
+			},
+			effect: {
+				charlotte: true,
+				onremove: true,
+				intro: {
+					content: "下一次使用火【杀】或【火攻】额外指定#个目标",
+				},
+				forced: true,
+				popup: false,
+				trigger: {
+					player: "useCard2",
+				},
+				filter(event, player) {
+					return get.info("dcsbronghuo").filter(event, player) && player.hasMark("dcsbronghuo_effect");
+				},
+				async content(event, trigger, player) {
+					let num = player.countMark(event.name);
+					player.removeSkill(event.name);
+					const { targets, card } = trigger;
+					const targetsx = game.filterPlayer(
+						target =>
+							!targets.includes(target) &&
+							lib.filter.targetEnabled2(card, player, target) &&
+							lib.filter.targetInRange(card, player, target)
+					);
+					if (targets?.length) {
+						num = Math.min(targetsx.length, num);
+						const result = await player
+							.chooseTarget(`融火：你可以为${get.translation(card)}增加至多${num}名目标`, [1, num], (card, player, target) =>
+								get.event().targets.includes(target)
+							)
+							.set("targets", targetsx)
+							.set("card", card)
+							.set("ai", target => get.effect(target, get.event().card, get.player(), get.player()))
+							.forResult();
+						if (result?.bool) {
+							const { targets } = result;
+							player.logSkill("dcsbronghuo", targets);
+							game.log(targets, "成为了", card, "的额外目标");
+							trigger.targets.addArray(targets);
+						}
+					}
+				},
+			},
+		},
 	},
 	dcsbyingmou: {
 		mark: true,
